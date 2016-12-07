bc9610a5a830625aad60149dcab7aa140f394979
jeromeBC <jerome.baroncampos@gmail.com>
2016-12-07 08:39:41 +0100

    ajout des classes

---
+++ vehicule.cpp
@@ -0,0 +1,40 @@
+#include "vehicule.h"
+
+Vehicule::Vehicule()
+{
+}
+Vehicule::Vehicule(int leId, int lImmat)
+{
+    id=leId;
+    immat=lImmat;
+}
+int Vehicule::getid() const
+{
+    return id;
+}
+
+void Vehicule::setid(int value)
+{
+    id = value;
+}
+int Vehicule::getimmat() const
+{
+    return immat;
+}
+
+void Vehicule::setimmat(int value)
+{
+    immat = value;
+}
+QString Vehicule::getEtat() const
+{
+    return etat;
+}
+
+void Vehicule::setEtat(const QString &value)
+{
+    etat = value;
+}
+
+
+
